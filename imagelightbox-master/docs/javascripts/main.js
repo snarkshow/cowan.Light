495e9698f4d57d24113398b2557896ff7bbe95eb
snarkshow <laura.hw.chuang@gmail.com>
2019-03-22 16:40:10 -0400

    figured out the lightbox

---
+++ imagelightbox-master/docs/javascripts/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+    $('a[data-imagelightbox="a"]').imageLightbox({
+        activity: true
+    });
+
+    $('a[data-imagelightbox="allowedtypes"]').imageLightbox({
+        allowedTypes: "gif"
+    });
+
+    $('a[data-imagelightbox="b"]').imageLightbox({
+        overlay: true
+    });
+
+    $('a[data-imagelightbox="c"]').imageLightbox({
+        button: true,
+        quitOnDocClick: false
+    });
+
+    $('a[data-imagelightbox="d"]').imageLightbox({
+        caption: true
+    });
+
+    $('a[data-imagelightbox="e"]').imageLightbox({
+        navigation: true,
+        selector: 'a[data-imagelightbox="e"]'
+    });
+
+    $('a[data-imagelightbox="f"]').imageLightbox({
+        arrows: true
+    });
+
+    $('a[data-imagelightbox="fullscreen"]').imageLightbox({
+        fullscreen: true
+    });
+
+    $('a[data-imagelightbox="g"]').imageLightbox({
+        activity: true,
+        arrows: true,
+        button: true,
+        caption: true,
+        navigation: true,
+        overlay: true,
+        quitOnDocClick: false,
+        selector: 'a[data-imagelightbox="f"]'
+    });
+
+    /**
+     *
+     */
+    var gallery = $('a[data-imagelightbox="h"]').imageLightbox({
+        arrows: true
+    });
+    $('.trigger_lightbox').on('click', function () {
+        gallery.startImageLightbox();
+    });
+
+    /**
+     * dynamically adding more images
+     */
+    var instanceI = $('a[data-imagelightbox="i"]').imageLightbox({
+        arrows: true
+    });
+    $(".add_image").on('click', function () {
+        var adding_ul = $(".demo_dynamic");
+        var li = $('<li></li>').appendTo( adding_ul );
+        var a = $("<a></a>")
+            .attr('data-imagelightbox',"i")
+            .attr('href', "images/demo4.jpg")
+            .appendTo( li );
+        $("<img />")
+            .attr("src", "images/thumb4.jpg")
+            .appendTo( a );
+        instanceI.addToImageLightbox( $("a[data-imagelightbox='i']") );
+    });
+
+    $('a[data-imagelightbox="j"]').imageLightbox({
+        history: true
+    });
+
+    $('a[data-imagelightbox="k"]').imageLightbox({
+        history: true
+    });
+
+    $('a[data-imagelightbox="video"]').imageLightbox({
+        activity: true,
+        arrows: true,
+        overlay: true
+    });
+
+    /**
+     *
+     */
+    $('a[data-imagelightbox="events"]').imageLightbox();
+    $(document)
+        .on("start.ilb2", function (_, e) {
+            console.log("start.ilb2");
+            console.log(e);
+        })
+        .on("quit.ilb2", function () {
+            console.log("quit.ilb2");
+        })
+        .on("loaded.ilb2", function () {
+            console.log("loaded.ilb2");
+        })
+        .on("previous.ilb2", function (_, e) {
+            console.log("previous.ilb2");
+            console.log(e);
+        })
+        .on("next.ilb2", function (_, e) {
+            console.log("next.ilb2");
+            console.log(e);
+        });
+});
---
+++ imagelightbox-master/gulpfile.js
@@ -0,0 +1,72 @@
+var gulp            = require('gulp'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    cleanCSS        = require('gulp-clean-css'),
+    connect         = require('gulp-connect'),
+    csslint         = require('gulp-csslint'),
+    eslint          = require('gulp-eslint'),
+    lintspaces      = require('gulp-lintspaces'),
+    rename          = require('gulp-rename'),
+    uglify          = require('gulp-uglify');
+
+gulp.task('csslint', function() {
+    return gulp.src('src/imagelightbox.css')
+        .pipe(csslint('.csslintrc'))
+        .pipe(csslint.formatter());
+});
+
+gulp.task('copy:css', gulp.series('csslint', function() {
+    return gulp.src('src/imagelightbox.css')
+        .pipe(gulp.dest('docs/stylesheets/'));
+}));
+
+gulp.task('minify:css', gulp.series('copy:css', function() {
+    return gulp.src('src/imagelightbox.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9', 'Firefox ESR', 'Android >= 2.3'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename('imagelightbox.min.css'))
+        .pipe(gulp.dest('dist/'));
+}));
+
+gulp.task('editorconfig', function() {
+    return gulp.src('src/imagelightbox.js')
+        .pipe(lintspaces({editorconfig: './.editorconfig'}))
+        .pipe(lintspaces.reporter());
+});
+
+gulp.task('eslint', function() {
+    return gulp.src('src/imagelightbox.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('copy:js',  gulp.series('editorconfig', 'eslint', function() {
+    return gulp.src('src/imagelightbox.js')
+        .pipe(gulp.dest('docs/javascripts/'));
+}));
+
+gulp.task('minify:js', gulp.series('copy:js', function() {
+    return gulp.src('src/imagelightbox.js')
+        .pipe(uglify())
+        .pipe(rename('imagelightbox.min.js'))
+        .pipe(gulp.dest('dist/'));
+}));
+
+gulp.task('build', gulp.parallel('minify:css', 'minify:js'));
+
+gulp.task('watch', function(done) {
+    gulp.watch(['docs/*.html','src/**/*'], gulp.series('build'));
+    done();
+});
+
+gulp.task('dev', gulp.parallel('build', 'watch', function(done) {
+    connect.server({
+        livereload: true
+    });
+    done();
+}));
+
+gulp.task('default', gulp.series('build'));
